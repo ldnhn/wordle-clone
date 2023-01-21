9c9ba4476cdf3cda23905b8e3eca5157e79a9516
ldnhn <62146587+ldnhn@users.noreply.github.com>
2023-01-22 03:01:31 +0700

    fix: keyboard viewing

--- src/App.jsx
+++ src/App.jsx
@@ -120,46 +120,108 @@ function App() {
           })}
         </div>
       </div>
-      <>
+
+      <Box
+        pos="fixed"
+        bottom={0}
+        mb={14}
+        left="50%"
+        className="keyboard-container"
+      >
         <Center>
-          <Flex>
-            <Box id="q">q</Box>
-            <Box id="q">w</Box>
-            <Box id="q">e</Box>
-            <Box id="q">r</Box>
-            <Box id="q">t</Box>
-            <Box id="q">y</Box>
-            <Box id="q">u</Box>
-            <Box id="q">i</Box>
-            <Box id="q">o</Box>
-            <Box id="q">p</Box>
+          <Flex gap={1} mt={1}>
+            <Box id="q" w={6} h={6} textAlign="center" rounded="sm">
+              q
+            </Box>
+            <Box id="w" w={6} h={6} textAlign="center" rounded="sm">
+              w
+            </Box>
+            <Box id="e" w={6} h={6} textAlign="center" rounded="sm">
+              e
+            </Box>
+            <Box id="r" w={6} h={6} textAlign="center" rounded="sm">
+              r
+            </Box>
+            <Box id="t" w={6} h={6} textAlign="center" rounded="sm">
+              t
+            </Box>
+            <Box id="y" w={6} h={6} textAlign="center" rounded="sm">
+              y
+            </Box>
+            <Box id="u" w={6} h={6} textAlign="center" rounded="sm">
+              u
+            </Box>
+            <Box id="i" w={6} h={6} textAlign="center" rounded="sm">
+              i
+            </Box>
+            <Box id="o" w={6} h={6} textAlign="center" rounded="sm">
+              o
+            </Box>
+            <Box id="p" w={6} h={6} textAlign="center" rounded="sm">
+              p
+            </Box>
           </Flex>
         </Center>
         <Center>
-          <Flex>
-            <Box id="a">a</Box>
-            <Box id="s">s</Box>
-            <Box id="d">d</Box>
-            <Box id="f">f</Box>
-            <Box id="g">g</Box>
-            <Box id="h">h</Box>
-            <Box id="j">j</Box>
-            <Box id="k">k</Box>
-            <Box id="l">l</Box>
+          <Flex gap={1} mt={1}>
+            <Box id="a" w={6} h={6} textAlign="center" rounded="sm">
+              a
+            </Box>
+            <Box id="s" w={6} h={6} textAlign="center" rounded="sm">
+              s
+            </Box>
+            <Box id="d" w={6} h={6} textAlign="center" rounded="sm">
+              d
+            </Box>
+            <Box id="f" w={6} h={6} textAlign="center" rounded="sm">
+              f
+            </Box>
+            <Box id="g" w={6} h={6} textAlign="center" rounded="sm">
+              g
+            </Box>
+            <Box id="h" w={6} h={6} textAlign="center" rounded="sm">
+              h
+            </Box>
+            <Box id="j" w={6} h={6} textAlign="center" rounded="sm">
+              j
+            </Box>
+            <Box id="k" w={6} h={6} textAlign="center" rounded="sm">
+              k
+            </Box>
+            <Box id="l" w={6} h={6} textAlign="center" rounded="sm">
+              l
+            </Box>
           </Flex>
         </Center>
         <Center>
-          <Flex>
-            <Box id="z">z</Box>
-            <Box id="x">x</Box>
-            <Box id="c">c</Box>
-            <Box id="v">v</Box>
-            <Box id="b">b</Box>
-            <Box id="n">n</Box>
-            <Box id="m">m</Box>
+          <Flex gap={1} mt={1}>
+            <Box id="z" w={6} h={6} textAlign="center" rounded="sm">
+              z
+            </Box>
+            <Box id="x" w={6} h={6} textAlign="center" rounded="sm">
+              x
+            </Box>
+            <Box id="c" w={6} h={6} textAlign="center" rounded="sm">
+              c
+            </Box>
+            <Box id="v" w={6} h={6} textAlign="center" rounded="sm">
+              v
+            </Box>
+            <Box id="b" w={6} h={6} textAlign="center" rounded="sm">
+              b
+            </Box>
+            <Box id="n" w={6} h={6} textAlign="center" rounded="sm">
+              n
+            </Box>
+            <Box id="m" w={6} h={6} textAlign="center" rounded="sm">
+              m
+            </Box>
           </Flex>
         </Center>
-      </>
+      </Box>
+      <Center pos="fixed" left="49%" bottom={0} pb={1}>
+        by nl
+      </Center>
     </div>
   );
 }
@@ -197,10 +259,16 @@ function Line({ guess, isFinal, chosenWord }) {
         // add colors
         if (char === chosenWord[i]) {
           className += " correct";
+          document.getElementById(char).style.background = "#5a9c51";
+          document.getElementById(char).style.color = "white";
         } else if (chosenWord.includes(char)) {
           className += " close";
+          document.getElementById(char).style.background = "#bea647";
+          document.getElementById(char).style.color = "white";
         } else {
           className += " incorrect";
+          document.getElementById(char).style.background = "#65696b";
+          document.getElementById(char).style.color = "white";
         }
       }
     }
